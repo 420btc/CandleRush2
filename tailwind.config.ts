333113ed542b284655054bec3e4cb5bfa4d80a01
Not Found Ink <notfoundink@example.com>
2025-05-11 06:27:52 +0200

    feat: add how-to-play page with game tips and slower scroll animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -80,7 +80,7 @@ const config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        'skew-scroll': 'skew-scroll 20s linear infinite',
+        'skew-scroll': 'skew-scroll 40s linear infinite',
       },
     },
   },
